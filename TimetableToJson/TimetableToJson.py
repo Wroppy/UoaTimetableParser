65d90a9b85d266839a3840a33f2b66dae79d2488
Weyman Wong <wxwong806@gmail.com>
2024-05-06 17:51:24 +1200

    Added initial packages and dependencies for scraping the timetable

---
+++ TimetableToJson/TimetableToJson.py
@@ -0,0 +1,7 @@
+class TimetableToJson:
+    def __init__(self):
+        print("Hello world")
+
+    def timetable_to_json(self, filename: str):
+        pass
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from TimetableToJson import TimetableToJson
+
+if __name__ == '__main__':
+    timetable_parser = TimetableToJson()
